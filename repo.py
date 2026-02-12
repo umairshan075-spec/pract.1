451ccd025167172f964b1a87e6ff965d61f3515b
Kishan R <thisiskishanr@gmail.com>
2026-02-12 22:29:25 +0530

    test2

--- repo.py
+++ repo.py
@@ -1,6 +1,6 @@
 a="shan shab"
 print(a)
-print("i am trying to push my code to repo as practice : ")
+print("i am trying to push my code to repo to practice : ")
 print("ma new skill sikh kr he rhoon ga abhi to bs repo sikha ha .")
 print("dakh lana start ha ya abhi")
 print("umair is paglet")
